c87141f60b92aa87584cf4c9b59a56c196ab06f8
vibraniumforge <gregm9999@gmail.com>
2019-09-16 13:45:17 -0700

    refactored again

--- solution.rb
+++ solution.rb
@@ -10,12 +10,11 @@ end
 
 
 def two_sum(numbers, target)
-	 for i in (0..numbers.length - 1)
-		for j in ((i + 1)..numbers.length - 1)
-			if numbers[i] + numbers[j] == target
-				p "{index1: #{i}, index2: #{j}}"
-			end
-		end
-	end
+  for i in (0..numbers.length)
+    for j in (i + 1)..numbers.length - 1)
+      if numbers[i] + numbers[j] == target
+        p "{index1: #{i}, index2: #{j}}"
+      end
+    end
+  end
 end
-
