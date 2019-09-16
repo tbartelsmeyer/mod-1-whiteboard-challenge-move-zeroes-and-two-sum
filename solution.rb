e72c2070a257c0de8f3dc81dc7a7af756f15cf77
vibraniumforge <gregm9999@gmail.com>
2019-09-16 13:39:46 -0700

    cleanup

---
+++ solution.rb
@@ -0,0 +1,21 @@
+def move_zeroes(nums)
+  for i in (0..nums.length)
+    if nums[i] == 0
+      nums << nums.slice!(i, 1).first
+    end
+  end
+  nums
+end
+  
+
+
+def two_sum(numbers, target)
+	 for i in (0..numbers.length - 1)
+		for j in ((i + 1)..numbers.length - 1)
+			if numbers[i] + numbers[j] == target
+				p "{index1: #{i}, index2: #{j}}"
+			end
+		end
+	end
+end
+
